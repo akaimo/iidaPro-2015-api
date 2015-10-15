5f7141b6bcfcd7146220822c91651837d6506736
shuhei <kawashu0723@gmail.com>
2015-10-15 18:11:35 +0900

    add trashes migrate

--- db/migrate/20151015074648_create_trashes.rb
+++ db/migrate/20151015074648_create_trashes.rb
@@ -1,7 +1,10 @@
 class CreateTrashes < ActiveRecord::Migration
   def change
     create_table :trashes do |t|
-
+      t.string :title, null: false
+      t.string :read, null: false
+      t.integer :category_id, null: false, default: 0
+      t.integer :knowledge
       t.timestamps null: false
     end
   end
