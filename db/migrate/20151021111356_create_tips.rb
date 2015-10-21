a7e71f8c005dcfda1e513252a19c3ced533732a3
shuhei <kawashu0723@gmail.com>
2015-10-21 20:28:25 +0900

    add tips migrate

--- db/migrate/20151021111356_create_tips.rb
+++ db/migrate/20151021111356_create_tips.rb
@@ -1,7 +1,9 @@
 class CreateTips < ActiveRecord::Migration
   def change
     create_table :tips do |t|
-
+      t.string :title, null: false
+      t.string :detail, null: false
+      t.string :genre, null: false
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015074648) do
+ActiveRecord::Schema.define(version: 20151021111356) do
+
+  create_table "tips", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "detail",     null: false
+    t.string   "genre",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trashes", force: :cascade do |t|
     t.string   "title",        null: false
