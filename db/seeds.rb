dc6d1d4bda076217112ab13d0896f5187f41ae92
shuhei <kawashu0723@gmail.com>
2015-11-15 17:24:32 +0900

    add district.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-table_names = %w(trashes, tips) # 適宜書き換える
+table_names = %w(trashes tips)
 table_names.each do |table_name|
   path = Rails.root.join("db/seeds", Rails.env, table_name + ".rb")
   if File.exist?(path)
